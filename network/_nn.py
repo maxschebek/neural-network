38d00f1fcb95324ca8e8a607adb5e72a43520c42
Maximilian Schebek <schebek@sol49.physik.hu-berlin.de>
2021-11-28 13:37:36 +0100

    add backpropagation

--- network/_nn.py
+++ network/_nn.py
@@ -5,33 +5,52 @@ from numpy.lib.function_base import append
 
 
 class Sequential:
-    def __init__(self, layer_dimensions: List[int]):
-        self.n_layers = len(layer_dimensions)
-        self.n_matrices = self.n_layers - 1
+    def __init__(self, layer_dims: List[int]):
+        self.n_layers = len(layer_dims)
+        self.n_weights = self.n_layers - 1
         np.random.seed(seed=0)
-        self.matrix_dimensions = [(layer_dimensions[i + 1], layer_dimensions[i] + 1) for i in range(self.n_matrices)]
-        self.weight_matrices = [
-            np.random.uniform(-1, 1, self.matrix_dimensions[i])
-            for i in range(self.n_matrices)
-                                            ]
+        self.weight_dims = [
+            (layer_dims[i + 1], layer_dims[i] + 1)
+            for i in range(self.n_weights)
+        ]
+        self.weights = [
+            np.random.uniform(-1, 1, self.weight_dims[i])
+            for i in range(self.n_weights)
+        ]
 
 
     def propagate_forward(self, input: np.ndarray) -> np.ndarray:
         self.layers = [np.append(1, input)]
-        for i_layer in range(1,self.n_layers ):
-            z = np.matmul(self.weight_matrices[i_layer-1], self.layers[i_layer-1])
+        self.layers_b = [np.zeros(1)]
+        for i_layer in range(1, self.n_layers):
+            z = np.matmul(self.weights[i_layer - 1], self.layers[i_layer - 1])
             a = np.array(list(map(sigmoid, z)))
+            b = np.array(list(map(sigmoid_derivative, z)))
             if i_layer != self.n_layers - 1:
-                self.layers.append(np.append(1, a)) 
+                self.layers.append(np.append(1, a))
+                self.layers_b.append(np.append(1, b))
             else:
-                 self.layers.append(a)                
+                self.layers.append(a)
+                self.layers_b.append(np.zeros(1))
         return self.layers[-1]
 
-    def propagate_backward(self,y_pred: np.ndarray,y_train):
-        diff = y_pred - y_train 
-        for i_matrix in reversed(range(self.n_matrices)):
-            pass
-            
+    def propagate_backward(self, error: np.ndarray):
+        self.layer_errors = [None] * self.n_layers
+        self.weight_errors = [None] * (self.n_layers - 1)
+
+        self.layer_errors[-1] = error
+        for i_lay in reversed(range(1, self.n_layers - 1)):
+            aux_vector = np.matmul(
+                self.weights[i_lay].T, self.layer_errors[i_lay + 1]
+            )
+            self.layer_errors[i_lay] = np.multiply(aux_vector, self.layers_b[i_lay])[1:]
+
+        self.weight_errors[-1] = np.outer(self.layer_errors[-1], self.layers[-2])
+        for i_lay in range(self.n_layers - 2):
+            self.weight_errors[i_lay] = np.outer(self.layer_errors[i_lay + 1], self.layers[i_lay])
+
+       
+   
 
     def train(self, input: np.ndarray) -> np.ndarray:
         self.propagate_forward(input)
--- tests/test_network.py
+++ tests/test_network.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import shape
 from network import Sequential, sigmoid
 import numpy as np
 
@@ -5,19 +6,45 @@ import numpy as np
 def test_propagate_forward():
     model = Sequential([2, 5, 1])
     input = np.array([0.7, 0.5])
-    prediction_model = model.propagate_forward(input)
+    weights = 2 * [None]
+    layers = 3 * [None]
 
     np.random.seed(seed=0)
-    weights_a = np.random.uniform(-1, 1, (5, 3))
-    weights_b = np.random.uniform(-1, 1, (1, 6))
-    layer0 = np.append(1, input)
-    z1 = np.matmul(weights_a, layer0)
+    weights[0] = np.random.uniform(-1, 1, (5, 3))
+    weights[1] = np.random.uniform(-1, 1, (1, 6))
+    layers[0] = np.append(1, input)
+    z1 = np.matmul(weights[0], layers[0])
     a1 = np.array(list(map(sigmoid, z1)))
-    layer1 = np.append(1, a1)
-    z2 = np.matmul(weights_b, layer1)
-    prediction = np.array(list(map(sigmoid, z2)))
+    layers[1] = np.append(1, a1)
+    z2 = np.matmul(weights[1], layers[1])
+    layers[2] = np.array(list(map(sigmoid, z2)))
 
-    assert all(model.layers[1] == layer1)
-    assert all(model.layers[2] == prediction)
-    assert prediction_model == prediction
+    model.propagate_forward(input)
+    assert np.array_equal(model.layers[0], layers[0])
+    assert np.array_equal(model.layers[1], layers[1])
+    assert np.array_equal(model.layers[2], layers[2])
+    assert np.array_equal(model.weights[0], weights[0])
+    assert np.array_equal(model.weights[1], weights[1])
 
+
+def test_propagate_backward():
+    model = Sequential([2, 5, 1])
+    input = np.array([0.7, 0.5])
+    y_pred = model.propagate_forward(input)
+    y_train = np.array([0.4])
+    layer_errors = 3 * [None]
+    weight_errors = 2 * [None]
+
+    layer_errors[2] = y_pred - y_train
+    weight_errors[1] = np.outer(layer_errors[2], model.layers[1])
+    aux_vector = np.matmul(model.weights[1].T, layer_errors[2])
+    layer_errors[1] = np.multiply(aux_vector, model.layers_b[1])[1:]
+    weight_errors[0] = np.outer(layer_errors[1], model.layers[0])
+
+    model.propagate_backward(y_pred - y_train)
+    assert np.shape(model.weight_errors[0]) == np.shape(model.weight_errors[0])
+    assert np.shape(model.weight_errors[1]) == np.shape(model.weight_errors[1])
+    assert np.array_equal(layer_errors[2], model.layer_errors[2])
+    assert np.array_equal(layer_errors[1], model.layer_errors[1])
+    assert np.array_equal(weight_errors[1], model.weight_errors[1])
+    assert np.array_equal(weight_errors[0], model.weight_errors[0])
