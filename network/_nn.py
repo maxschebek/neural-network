e3b3bfe6b411f595f8e0f2a9fa293ed5d62979d3
Maximilian Schebek <schebek@physik.hu-berlin.de>
2021-12-05 19:16:21 +0100

    add accumulators and costfunction

--- network/_nn.py
+++ network/_nn.py
@@ -1,8 +1,5 @@
 import numpy as np
 from typing import List
-from numpy.core.numeric import ones
-
-from numpy.lib.function_base import append
 
 
 class Sequential:
@@ -11,57 +8,46 @@ class Sequential:
         self.n_weights = self.n_layers - 1
         np.random.seed(seed=0)
         self.weight_dims = [
-            (layer_dims[i + 1], layer_dims[i] + 1)
-            for i in range(self.n_weights)
+            (layer_dims[i + 1], layer_dims[i] + 1) for i in range(self.n_weights)
         ]
         self.weights = [
-            np.random.uniform(-1, 1, self.weight_dims[i])
-            for i in range(self.n_weights)
+            np.random.uniform(-1, 1, self.weight_dims[i]) for i in range(self.n_weights)
         ]
-        self.weight_errors = [
-            np.zeros(self.weight_dims[i])
-            for i in range(self.n_weights)
+        self.accumulators = [
+            np.zeros(self.weight_dims[i]) for i in range(self.n_weights)
         ]
-        
-        
         self.layers = [
-            np.ones(layer_dims[i_lay] + 1)
-            for i_lay in range(self.n_layers)
+            np.ones(layer_dims[i_lay] + 1) for i_lay in range(self.n_layers - 1)
         ]
+        self.layers.append(np.ones(layer_dims[-1]))
 
-        self.layers_b = [
-            np.zeros(layer_dims[i_lay])
-            for i_lay in range(self.n_layers)
-        ]
+        self.layers_b = [np.zeros(layer_dims[i_lay]) for i_lay in range(self.n_layers)]
         self.layer_errors = [
-            np.zeros(layer_dims[i_lay])
-            for i_lay in range(self.n_layers)
+            np.zeros(layer_dims[i_lay]) for i_lay in range(self.n_layers)
         ]
 
     def propagate_forward(self, input: np.ndarray) -> np.ndarray:
         self.layers[0][1:] = input
-        for i_lay in range(1, self.n_layers):
+        for i_lay in range(1, self.n_layers - 1):
             z = np.matmul(self.weights[i_lay - 1], self.layers[i_lay - 1])
-            a = np.array(list(map(sigmoid, z)))
-            b = np.array(list(map(sigmoid_derivative, z)))
-            self.layers[i_lay][1:] = a  
-            self.layers_b[i_lay] = b
-        return self.layers[-1][1:]
+            self.layers[i_lay][1:] = np.array(list(map(sigmoid, z)))
+            self.layers_b[i_lay] = np.array(list(map(sigmoid_derivative, z)))
+        # Last step explicit
+        z = np.matmul(self.weights[-1], self.layers[-2])
+        self.layers[-1] = np.array(list(map(sigmoid, z)))
+        self.layers_b[-1][:] = 0.0
+        return self.layers[-1]
 
     def propagate_backward(self, error: np.ndarray):
 
         self.layer_errors[-1] = error
-        self.weight_errors[-1] = np.outer(self.layer_errors[-1], self.layers[-2])
+        self.accumulators[-1] += np.outer(self.layer_errors[-1], self.layers[-2])
         for i_lay in reversed(range(1, self.n_layers - 1)):
-            aux_vector = np.matmul(
-                self.weights[i_lay][:,1:].T, self.layer_errors[i_lay + 1]
+            aux_vector = np.matmul(self.weights[i_lay].T, self.layer_errors[i_lay + 1])
+            self.layer_errors[i_lay] = np.multiply(aux_vector[1:], self.layers_b[i_lay])
+            self.accumulators[i_lay - 1] += np.outer(
+                self.layer_errors[i_lay], self.layers[i_lay - 1]
             )
-            self.layer_errors[i_lay] = np.multiply(aux_vector, self.layers_b[i_lay])
-            self.weight_errors[i_lay - 1 ] = np.outer(self.layer_errors[i_lay], self.layers[i_lay - 1])
-            
-
-       
-   
 
     def train(self, input: np.ndarray) -> np.ndarray:
         self.propagate_forward(input)
@@ -73,3 +59,13 @@ def sigmoid(x: float) -> float:
 
 def sigmoid_derivative(x: float) -> float:
     return sigmoid(x) * (1 - sigmoid(x))
+
+
+def costfunc(y, y_pred):
+    n_samples = np.shape(y)[0]
+    cost = 0
+    for isp in range(n_samples):
+        cost += -np.dot(y[isp, :], np.log(y_pred[isp, :])) - np.dot(
+            (1 - y[isp, :]), np.log(1 - y_pred[isp, :])
+        )
+    return 1 / n_samples * cost
